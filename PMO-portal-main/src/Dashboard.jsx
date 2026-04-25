cdd6140ac53245709b65193052444c2b6f379cd4
Varun-SK1 <n1346706@ntu.ac.uk>
2026-04-25 16:50:04 +0100

    Update Dashboard.jsx

--- PMO-portal-main/src/Dashboard.jsx
+++ PMO-portal-main/src/Dashboard.jsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <iframe
       title="PMO Dashboard"
-      src="https://app.powerbi.com/reportEmbed?reportId=02fac42c-b28e-4b0f-9927-bf6959e5374b&autoAuth=true&ctid=8acbc2c5-c8ed-42c7-8169-ba438a0dbe2f"
+      src="https://app.powerbi.com/reportEmbed?reportId=e705019d-e9cf-4908-9af3-2631964803e2&autoAuth=true&ctid=8acbc2c5-c8ed-42c7-8169-ba438a0dbe2f"
       frameBorder="0"
       allowFullScreen={true}
       style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none' }}
